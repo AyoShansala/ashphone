267f2482e695e9da030495e391c65cef0c8e11cf
AyoShansala <shansala.ayodya@gmail.com>
2022-09-07 13:02:45 +0530

    Ayodhya code

---
+++ option.py
@@ -0,0 +1,13 @@
+
+option = input("Type your option :")
+match option:
+    case "Good Morning":
+        print("hello Good Morning ")
+    case "Good Night":
+        print("Hello Good night")
+    case "How are you":
+        print("hello how are you")
+    case "How mutch is this":
+        print(" hello How mutch is this")  
+    case _:
+        print("wrong option")
